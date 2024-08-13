bacc5e82df58fb2f5679e8c40cd7e6ccc13ab165
Douglas Henrique <douglashenrique3721@gmail.com>
2024-08-13 20:57:48 -0300

    criada a função usuários

--- api.py
+++ api.py
@@ -1,14 +1,17 @@
 import requests
 import pandas as pd
 
+def api_usuarios():
 
 
-api_url = "https://fakestoreapi.com/users/5"
-response = requests.get(api_url)
-response.json()
-print("esse é o print do json")
-print(response.json())
-data = response.json()
-df = pd.DataFrame(data)
-print("esse é o print do dataframe")
-print(df)
+    api_url = "https://fakestoreapi.com/users/5"
+    response = requests.get(api_url)
+    response.json()
+    print("esse é o print do json")
+    print(response.json())
+    data = response.json()
+    df = pd.DataFrame(data)
+    print("esse é o print do dataframe")
+    print(df)
+
+api_usuarios()
